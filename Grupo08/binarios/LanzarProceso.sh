9f62bc8fc9725b17ea5b205441e4751ff10f4b9b
fedebrasburg <federico.brasburg@gmail.com>
2016-04-29 15:23:10 -0300

    Lanar proceso detecta quien lo llama. Valida el cero parametro

--- Grupo08/binarios/LanzarProceso.sh
+++ Grupo08/binarios/LanzarProceso.sh
@@ -1,46 +1,62 @@
 #Falta ver que este inicializado el ambiente
-#Antes de iniciarlo me fijo que no es te corriendo.
 proceso=$2
 background=$1
-
+parametroCorrecto=-b
 #Me fijo la cantidad de parametros
 dos=2
-if [ "$#" -gt "$dos" ]
+
+#Me fijo quien lo llama
+padre="$(ps -o comm= $PPID)"
+
+#Metodo de salida
+#$1 mensaje
+Mensaje () {
+    if [ "$padre" == "bash" ]
+    then
+   		echo "Lanzar Proceso:" $1
+   	else
+   		echo $1
+   	fi
+   	exit
+}
+
+if [ "$#" -gt "$dos" -o "$#" -eq 0 ]
 then
-	echo "Cantidad de parametros incorrecta"
-	exit
+	Mensaje "Cantidad de parametros incorrecta"
 fi
 if [ "$#" -eq "$dos" ]
 then
 	proceso=$2
 	background=$1
+	if [ ! "$background" == "$parametroCorrecto" ]
+	then
+		Mensaje "Flag incorrecto, el Unico posible es -b"
+	fi
 else
 	proceso=$1
 fi
 #Me fijo que exista el proceso
 if [ ! -f $proceso ]
 then
-	echo "Proceso no existe"
-	exit
+	Mensaje "Proceso no existe"
 fi
 #Me fijo que tenga permisos de ejecucion
 if [ ! -x $proceso ]
 then
-	echo "Proceso no tiene permisos de ejecucion"
-	exit
+	Mensaje "Proceso no tiene permisos de ejecucion"
 fi
+#Antes de iniciarlo me fijo que no es te corriendo.
 for i in $(ps -L u n )
 do
 	if [[ $i == $proceso ]]; then
-		echo Proceso $proceso ya se encuentra corriendo
-		exit
+		Mensaje "Proceso $proceso ya se encuentra corriendo"
 	fi
 done
 if [ $background == "-b" ]
 then
 	sh "$proceso" &
-	echo Inicializacion exitosa. El proceso $proceso esta corriendo en background. Numero de proceso: $!
+	Mensaje "Inicializacion exitosa. El proceso $proceso esta corriendo en background. Numero de proceso: $!"
 else
 	sh "$proceso"
-	echo Inicializacion exitosa. El proceso $proceso esta corriendo.
+	Mensaje "Inicializacion exitosa. El proceso $proceso esta corriendo."
 fi
