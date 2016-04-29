368ccb009ffb9949fa514c876d9bd836856fb271
Gaston Snaider <gastonsnaider93@gmail.com>
2016-04-29 02:41:37 -0300

    PrepararAmbiente listo para el camino feliz

--- Grupo08/binarios/PrepararAmbiente.sh
+++ Grupo08/binarios/PrepararAmbiente.sh
@@ -7,17 +7,33 @@
 #TODO Si es necesario reparar ambiente.
 	#TODO Crear directorio de recovery con los ejecutables para restaurarlos	
 
+
+GRABAR='perl GrabarBitacora.pl PrepararAmbiente'
 config_file="../config/CIPAL.cnf"
 IFS='='
 	while read var value user record_date
 	do
-		echo "variable: $var"
-		echo "valor: $value"
-		echo "usuario: $user"
-		echo "fecha: $record_date"
+		export "$var"="$value"
+		echo "Variable $var inicializada con valor $value"
+		$GRABAR "Variable $var inicializada con valor $value"
 	done < $config_file
+
+	echo "Estado del Sistema: INICIALIZADO"
+	$GRABAR "Estado del Sistema: INICIALIZADO"
+
+	read -p "¿Desea efectuar la activación de RecibirOfertas? Si – No: " reply
 	
-	#TODO Setear variables de entorno
+	while [[ ! $reply =~ ^[sS][iI]?$ ]] && [[ ! $reply =~ ^[nN][oO]?$ ]]
+	do
+		echo "Respuesta invalida."
+		read -p "¿Desea efectuar la activación de RecibirOfertas? Si – No: " reply
+	done
 
-#TODO preguntar por ejecucion de RecibirOfertas
-
+	if [[ $reply =~ ^[sS][iI]?$ ]];
+	then
+		echo "Lanzando RecibirOfertas"
+		$(./LanzarProceso.sh RecibirOfertas.sh)
+		echo "Para detener el proceso RecibirOfertas, se debe invocar el comando \"./DetenerProceso.sh RecibirOfertas.sh\""
+	else
+		echo "Para lanzar el proceso RecibirOfertas, se debe invocar el comando \"./LanzarProceso.sh RecibirOfertas.sh"
+	fi 
