3edafb1f4444d052457bd60c84c7def752b3eb58
Gaston Snaider <gastonsnaider93@gmail.com>
2016-04-29 00:51:56 -0300

    Obtengo datos de archivo de config

--- Grupo08/binarios/PrepararAmbiente.sh
+++ Grupo08/binarios/PrepararAmbiente.sh
@@ -7,12 +7,17 @@
 #TODO Si es necesario reparar ambiente.
 	#TODO Crear directorio de recovery con los ejecutables para restaurarlos	
 
-IFS='
-'
-for linea in `cat ../config/CIPAL.cnf`; do
-	echo "$linea"
+config_file="../config/CIPAL.cnf"
+IFS='='
+	while read var value user record_date
+	do
+		echo "variable: $var"
+		echo "valor: $value"
+		echo "usuario: $user"
+		echo "fecha: $record_date"
+	done < $config_file
+	
 	#TODO Setear variables de entorno
-done
 
 #TODO preguntar por ejecucion de RecibirOfertas
 
